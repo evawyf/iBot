88a7059ad966b0c5226892d90bfda095c60de3dc
Yifan Wang <evawyf@Abhimanyus-MacBook-Pro-2.local>
2024-10-29 13:46:06 -0700

    docker option for run_ngrok.sh

--- iBot/run_ngrok.sh
+++ iBot/run_ngrok.sh
@@ -6,6 +6,7 @@ ngrok config add-authtoken 1qpU7j7SxlDYCahmCom9l6H8dSZ_3MuoncJsBianKdL3TXLyp
 # Start ngrok tunnel on port 5678
 # ngrok http 4040
 ngrok http --url=ibot.ngrok.app 5678
+# docker run -it -e NGROK_AUTHTOKEN=1qpU7j7SxlDYCahmCom9l6H8dSZ_3MuoncJsBianKdL3TXLyp ngrok/ngrok http 5678 --url=ibot.ngrok.dev
 
 # run: chmod +x start_ngrok.sh
 # run: ./start_ngrok.sh
